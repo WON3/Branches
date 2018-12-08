c68c377377f0dece41e19c89b2e574b448952b9f
Justin Valdez <justinjvaldez@gmail.com>
2018-12-08 12:21:37 -0700

    shared

---
+++ src/Components/Shared/Buttons/Buttons.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import './Buttons.css';
+
+/*submit buttons, logout buttons, home button shared, links(genres, stories)*/
---
+++ src/Components/Shared/Header/Header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './Header.css';
+
+/* SETUP FOR HEADER =
+Nav bar, title, logout, ?profile icon or name?*/
