542405c34c0f235517c0f907711e671f2dbffa4b
Justin Valdez <justinjvaldez@gmail.com>
2018-12-08 15:23:25 -0700

    shared

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Buttons from './Components/Shared/Buttons/Buttons';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+          <Buttons/>
           HELLO FOO PEOPLE
 
       </div>
--- src/Components/Shared/Buttons/Buttons.js
+++ src/Components/Shared/Buttons/Buttons.js
@@ -1,4 +1,23 @@
 import React from 'react';
 import './Buttons.css';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import { ButtonBase } from '@material-ui/core';
 
-/*submit buttons, logout buttons, home button shared, links(genres, stories)*/
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
+
+export default Buttons;
+
+
+
+
+
+/*submit buttons, logout buttons, home button shared, */
