e9b2356ee9c090bcdd57e0c20f767ad1b6ec6885
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-12-09 18:40:27 -0700

    Coded redux skeleton, added some filler to be adjusted

---
+++ src/ducks/reducer.js
@@ -0,0 +1,67 @@
+const intialState = {
+    //user
+    userName:'',
+    userId:'',
+    
+    //story
+    storyId:'',
+    storyBody:'',
+    storyAddition:''
+
+}
+
+const GET_USER = 'GET_USER';
+const GET_STORY = 'GET_STORY';
+const STORY_ADDITION = 'STORY_ADDITION';
+
+function reducer (state = initialState, action) {
+    let {payload} = action;
+
+    switch (action.type) {
+        case GET_USER:
+            return Object.assign({}, state, {
+                userId: payload.userId,
+                userName: payload.userName
+            });
+        case GET_STORY:
+            return Object.assign({}, state, {
+                storyId: payload.storyId,
+                storyBody: payload.storyBody
+            });
+        default:
+            return state;
+    }
+}
+
+/////////////////////User///////////////////////////
+export function getUser (userId, userName) {
+    return {
+        type: GET_USER,
+        payload: {
+            userId,
+            userName
+        }
+    }
+}
+
+
+///////////////Story//////////////////////
+export function getStory (storyId, storyBody) {
+    return {
+        type: GET_STORY,
+        payload: {
+            storyId,
+            storyBody
+        }
+    }
+}
+export function storyAddition (storyAddition) {
+    return {
+        type: STORY_ADDITION,
+        payload: {
+            storyAddition
+        }
+    }
+}
+
+export default reducer;
