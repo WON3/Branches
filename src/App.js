f120971be51e355c9733ba62443d38d15290681d
Daniel Valentine <mrdanvalentine@gmail.com>
2018-12-10 23:05:13 -0500

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from './Components/Shared/Buttons/Buttons';
 
 
 class App extends Component {
@@ -9,12 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
         <h1>Branches</h1>
-          <img src={logo} className="App-logo" alt="logo" />
         </header>
         
-          <Buttons/>
-          HELLO FOO PEOPLE
-          <Button/>
       </div>
     );
   }
