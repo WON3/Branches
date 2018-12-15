356d85f220ba9768e297d1e946bce88f953ad8a2
Levi DeJean <levidejean@gmail.com>
2018-12-15 13:21:54 -0600

    fixing merge conflicts

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Header from "./Components/Shared/Header/Header";
 
 class App extends Component {
   render() {
-<<<<<<< HEAD
 
     return (
       <div className="App">
@@ -17,16 +16,6 @@ class App extends Component {
           {routes}
 
         </BrowserRouter>
-=======
-    //just inserted CreateStory temporarily until routes are completed
-    return (
-      <div className="App">
-        <header className="App-header">
-        <h1>Branches</h1>
-        
-        </header>
-      
->>>>>>> 97e6358b5f6b7ea0995fb6258e66181155d9c6bd
       </div>
     );
   }
