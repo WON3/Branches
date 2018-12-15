0b568745b73a15338c2d92c620cc8eeb3623ee68
Levi DeJean <levidejean@gmail.com>
2018-12-15 13:14:14 -0600

    building nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./Components/Views/NavBar/NavBar";
+import { BrowserRouter } from "react-router-dom";
+import routes from "./routes";
+import Header from "./Components/Shared/Header/Header";
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-        <h1>Branches</h1>
-        </header>
-        
+        <Header />
+
+        <BrowserRouter>
+          {routes}
+
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/Components/Views/View_Story/View_Story.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ViewStory extends Component{
+    render(){
+        return(
+            <div>View Story</div>
+        )
+    }
+}
+
+export default ViewStory;
