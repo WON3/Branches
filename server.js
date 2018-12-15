5517e3899c2083028584cfe5069e600168e1021c
Tanner Bryant <tannerbryant@gmail.com>
2018-12-15 10:02:23 -0700

    tb: Adding endpoints for getting contributions.

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ require('dotenv').config();
 /// Serving static files. 
 app.use(express.static(path.join(__dirname, '/build')));
 
-
 massive(process.env.DATABASE_URL)
     .then(db => {
         app.set('db', db);
@@ -22,6 +21,37 @@ massive(process.env.DATABASE_URL)
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get('/api/contributions/:story_id', (req, res) => {
+    const db = req.app.get('db');
+    const { story_id } = req.params
+    const data = {}
+    db.get_contributions({
+        story_id
+    })
+    .then(result => {
+        data.contributions = result
+            // Using findOne() get story title and description
+            return db.stories.findOne({
+                story_id
+            })   // return promise
+
+        })
+        .then(result => {
+            data.story = {
+                title: result.title,
+                description: result.description
+            }
+            res.send(data)
+        })
+    // .then() will contain story title and description
+    // assign data.storyDetails = result from .then() 
+    // send data back. res.send
+    .catch(error=>{
+        console.error("error getting story.")
+        res.status(500)
+    })
+})
 
 
 /// Catch all for routing
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Button from './Components/Shared/Buttons/Buttons';
+import Contribute from './Components/Views/Contribute/Contribute'
 
 
 class App extends Component {
@@ -9,12 +10,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
         <h1>Branches</h1>
-          <img src={logo} className="App-logo" alt="logo" />
         </header>
-        
-          <Buttons/>
+          <Button/>
           HELLO FOO PEOPLE
           <Button/>
+        <Contribute/>
       </div>
     );
   }
