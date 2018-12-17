9e8b52fb4b93025c7433a87713879275c7585470
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-12-17 06:59:29 -0700

    Resoved merge conflicts

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        <h1>Branches</h1>
-        <Register />
-        </header>
+       
         
       </div>
     );
