681969d2b7a9f034015aa02b018638b15717c2f2
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-12-17 07:05:01 -0700

    Conflicts resolved

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import Register from './Components/Views/Register/Register';
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./Components/Views/NavBar/NavBar";
+import { BrowserRouter } from "react-router-dom";
+import routes from "./routes";
+import Header from "./Components/Shared/Header/Header";
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
-       
+        <Header />
 
+        <BrowserRouter>
+          {routes}
 
-
-
-
-      
-        
+        </BrowserRouter>
       </div>
     );
   }
