1afc8232245a41f5c5136b6667a80fb0db02a920
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-12-18 21:42:44 -0700

    Coded user card, created an avatar component card component.

--- src/Components/Views/Login/Login.js
+++ src/Components/Views/Login/Login.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import Register from '../Register/Register';
+
+import {connect} from 'react-redux';
+import {getUser} from '../../../ducks/reducer';
+
 
 class Login extends Component{
     render(){
         return(
-            <div>Login</div>
+            <div>
+                <h1>hi</h1>
+                <Register/>
+            </div>
         )
     }
 }
 
-export default Login;
+export default connect(null, {getUser})(Login);
--- src/Components/Views/User/User.js
+++ src/Components/Views/User/User.js
@@ -1,9 +1,38 @@
 import React, {Component} from 'react';
 
+import ProfileCard from './ProfileCard';
+import { FormHelperText } from '@material-ui/core';
+
+
+//Need Redux initialState for username, user Badge, profile pic, stories in work
+
 class User extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+    
+    
+    
     render(){
         return(
-            <div>User</div>
+            <div style={{display:'flex',justifyContent:'space-around'}}>
+                <ProfileCard 
+                    name='Ry'
+                />
+                <ProfileCard 
+                    name='Tris'
+                />
+                <ProfileCard 
+                    name='Pais'
+                />
+                <div>current works</div>
+                <div>profile</div>
+
+
+            </div>
         )
     }
 }
