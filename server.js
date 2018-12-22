b2be07cc3dcc4903b87a41567efd0b466048a27a
Tanner Bryant <tannerbryant@gmail.com>
2018-12-22 09:24:05 -0700

    Feature-009 Contribute FE

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 /// Serving static files.
 const express = require("express"),
-  cors = require("cors"),
-  bodyParser = require("body-parser"),
-  massive = require("massive"),
-  path = require("path"),
-  app = express();
-  session = require("express-session");
-  passport = require("passport");
-  LocalStrategy = require("passport-local").Strategy;
-  bcrypt = require("bcrypt");
-  contribution = require('./server/controllers/contributionController'),
+    cors = require("cors"),
+    bodyParser = require("body-parser"),
+    massive = require("massive"),
+    path = require("path"),
+    app = express(),
+    session = require("express-session"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local").Strategy,
+    bcrypt = require("bcrypt"),
+    contribution = require('./server/controllers/contributionController'),
     story = require('./server/controllers/storyController'),
     user = require('./server/controllers/userController'),
-    admin = require('./server/controllers/adminController'),
-    passport = require('passport');
-    LocalStrategy = require('passport-local').Strategy;
-    bcrypt = require('bcrypt'),
-    session = require('express-session');
+    admin = require('./server/controllers/adminController')
 
-require("dotenv").config();
+    require("dotenv").config();
 
 /// Serving static files.
 massive(process.env.DATABASE_URL)
@@ -30,59 +26,59 @@ massive(process.env.DATABASE_URL)
     })
 ////////////////////Passport authenticate///////////////////////////
 app.use(session({
-    secret:process.env.SESSION_SECRET
+    secret: process.env.SESSION_SECRET
 }))
 
-app.use( passport.initialize() );
-app.use( passport.session() );
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
 passport.use('register', new LocalStrategy({
     usernameField: 'email',
     passReqToCallback: true,
-}, (req,email, username, done) => {
-        const db = req.app.get('db');
-        let locUser =null;
-        db.users.findOne({email:email})
+}, (req, email, username, done) => {
+    const db = req.app.get('db');
+    let locUser = null;
+    db.users.findOne({ email: email })
         .then(user => {
-            if(!user){
-                db.users.insert({username: req.body.username,email:req.body.email})
-                .then(userid => {
-                    locUser = userid;
-                    return bcrypt.hash(req.body.password,10)
-                })
-                .then(pass => {
-                    return db.user_login.insert({user_id:locUser.id, login_token:pass})
-                })
-                .then(user => {
-                    done(null, locUser);
-                })
+            if (!user) {
+                db.users.insert({ username: req.body.username, email: req.body.email })
+                    .then(userid => {
+                        locUser = userid;
+                        return bcrypt.hash(req.body.password, 10)
+                    })
+                    .then(pass => {
+                        return db.user_login.insert({ user_id: locUser.id, login_token: pass })
+                    })
+                    .then(user => {
+                        done(null, locUser);
+                    })
             } else {
                 done('User with this email already exists');
-            } 
+            }
         })
         .catch(err => {
             done(err);
         });
-                        
-    }
+
+}
 ));
 
-passport.serializeUser((user,done)=> {
-    if(!user){
+passport.serializeUser((user, done) => {
+    if (!user) {
         done('No user');
     }
 
     done(null, user);
-    },
+},
 );
 
 passport.deserializeUser((user, done) => {
     done(null, user);
 });
 
-app.post('/login', passport.authenticate(['login']), (req, res, next)=>{
+app.post('/login', passport.authenticate(['login']), (req, res, next) => {
     res.send('Successful Login!')
 })
 
@@ -94,72 +90,65 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-let attempts=0;
+let attempts = 0;
 
 passport.use('login', new LocalStrategy({
-    usernameField:'email',
-    passReqToCallback:true,
+    usernameField: 'email',
+    passReqToCallback: true,
 }, (req, email, password, done) => {
-    if(attempt >= 3){
+    if (attempt >= 3) {
         done('Sorry this account is locked. Please update password to unlock')
-    }else{
-        const db =req.app.get('db')
-        db.user_usertable.findOne({email:email})
-        .then(user => {
-            if(!user) {
-                bcrypt.hash(password, 10)
-                .then((password)=>{
-                    return db.user_table.insert({email, password})
-                })
-                .then((user)=>{
+    } else {
+        const db = req.app.get('db')
+        db.user_usertable.findOne({ email: email })
+            .then(user => {
+                if (!user) {
+                    bcrypt.hash(password, 10)
+                        .then((password) => {
+                            return db.user_table.insert({ email, password })
+                        })
+                        .then((user) => {
+                            delete user.password;
+                            done(null, user);
+                        })
+                } else if (!bcrypt.compareSync(password, user.password)) {
+                    attempts++
+
+                    return done('Invalid email or password');
+                } else {
                     delete user.password;
                     done(null, user);
-                })
-            }else if(!bcrypt.compareSync(password, user.password)){
-                attempts++
-
-                return done('Invalid email or password');
-            }else{
-                delete user.password;
-                done(null, user);
-            }
-        })
-        .catch(err => {
-            done(err);
-        });
-}
+                }
+            })
+            .catch(err => {
+                done(err);
+            });
+    }
 
 }));
 
 passport.serializeUser((user, done) => {
-if (!user) {
-    done('No user');
-}
+    if (!user) {
+        done('No user');
+    }
 
-done(null, user);
+    done(null, user);
 },
 );
 
 passport.deserializeUser((user, done) => {
-done(null, user);
+    done(null, user);
 });
 
 
-app.post('/login', passport.authenticate(['login']), (req, res, next)=>{
-res.send('Successful Login!')
+app.post('/login', passport.authenticate(['login']), (req, res, next) => {
+    res.send('Successful Login!')
 })
-
-/// Catch all for routing
-app.get("/*", (req, res) => {
-  res.sendFile("index.html", {
-    root: path.join(__dirname, "build")
-  });
-});
 
 /////////////////// API ROUTES ///////////////////////////
 
 app.get('/api/contributions/:story_id', contribution.get_contribution)
-app.post('/api/register', passport.authenticate(['register']), (req, res, next)=>{
+app.post('/api/register', passport.authenticate(['register']), (req, res, next) => {
     res.send('Successful Login!')
 })
 
--- src/Components/Views/View_Story/View_Story.js
+++ src/Components/Views/View_Story/View_Story.js
@@ -1,11 +1,53 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import './View_Story.css';
+import RenderCont from './RenderCont'
 
-class ViewStory extends Component{
-    render(){
-        return(
-            <div>View Story</div>
+
+class ViewStory extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            contribution: {},
+            open: false
+        }
+    }
+    handleTooltipClose = () => {
+        this.setState({ open: false });
+    };
+
+
+    handleTooltipOpen = () => {
+        this.setState({ open: true });
+    };
+    componentDidMount() {
+        const { story_id } = this.props.match.params
+        axios.get(`/api/contributions/${story_id}`)
+            .then((res) =>
+                this.setState({ contribution: res.data })
+            )
+            .catch(err => console.log('axios create error', err))
+    }
+
+    render() {
+        if (!this.state.contribution.story) {
+            return <div>Loading</div>
+        }
+
+        const contributions = this.state.contribution.contributions.map((contribution) => <RenderCont contribution={contribution} />)
+
+        return (
+            <div className="body">
+                <div className="head">
+                    <h1>"{this.state.contribution.story.title}"</h1>
+                    <h3>{this.state.contribution.story.description}</h3>
+                </div>
+                <div>{contributions}</div>
+            </div>
         )
     }
 }
+
 
 export default ViewStory;
