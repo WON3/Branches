baf83ecf6f78d85a80014ece03162d3aadbe21ec
Justin Valdez <justinjvaldez@gmail.com>
2018-12-22 17:12:48 -0700

    merged with develop

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import NavBar from "./Components/Shared/NavBar/NavBar";
 import { BrowserRouter } from "react-router-dom";
 import routes from "./routes";
 import Header from "./Components/Shared/Header/Header";
-import Login from "./Components/Views/Login/Login";
 
 class App extends Component {
   render() {
