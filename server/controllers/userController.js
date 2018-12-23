4cd32fe461a2f4723dabc89c4b24fbaf450dc917
rfilaxwy <ryanfilaxwylie@gmail.com>
2018-12-22 22:26:10 -0700

    BE for get profile done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,8 +2,9 @@ module.exports = {
     getProfile: (req,res) =>{
         const db = req.app.get('db');
         const {userId} = req.params;
-        db.user_usertable.findOne({id:userId})
+        db.getProfile(userId)
             .then(response=>{
+                console.log(response)
                 res.status(200)
                     .send(response.data);
             }).catch(err=>{
