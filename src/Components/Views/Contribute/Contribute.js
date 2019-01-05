c440c73628c53df028d9fd0dabde40d9088aaa0f
Tanner Bryant <tannerbryant@gmail.com>
2019-01-05 10:32:57 -0700

    feature 101 style

--- src/Components/Views/Contribute/Contribute.js
+++ src/Components/Views/Contribute/Contribute.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import axios from 'axios';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import classNames from 'classnames';
 import './Contribute.css'
 
 class Contribute extends React.Component {
@@ -10,17 +13,16 @@ class Contribute extends React.Component {
             story_id: 1,
             contribution: "",
             is_accepted: false,
-            prior_contribution_id: 0
+            prior_contribution_id: 0,
+            multiline: 'Controlled'
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-
     }
     handleChange(e) {
         this.setState({
             contribution: e.target.value,
         })
-        console.log(this.state.contribution)
     }
     handleSubmit(e) {
         e.preventDefault();
@@ -35,10 +37,10 @@ class Contribute extends React.Component {
             }
             debugger
             axios.post('/api/contribution', contributions)
-            .then(res => {
-                this.props.addContribution(res.data);
-            })
-            .catch(err => console.log("ya done fudged up", err))
+                .then(res => {
+                    this.props.addContribution(res.data);
+                })
+                .catch(err => console.log("ya done fudged up", err))
         } else {
             console.log('Put something in the field!!')
         }
@@ -46,13 +48,19 @@ class Contribute extends React.Component {
 
 
     render() {
-
+        const { classes } = this.props;
         return (
             <div className="contribute">
-                <form>
-                <span>Add contribution</span>
-                <input type="text" value={this.state.contribution} onChange={this.handleChange}/>
-                <button onClick={this.handleSubmit}>Submit Contribution</button>
+                <form noValidate autoComplete="off">
+                    <h1>Add contribution</h1>
+                    <TextField id="outlined-multiline-flexible"
+                        label="Continue the story here!"
+                        multiline
+                        rowsMax="4"
+                        margin="normal"
+                        variant="outlined" 
+                        onChange={this.handleChange} />
+                    <Button style={{ margin: "auto" }} size="large" color="default" onClick={this.handleSubmit}>Submit Contribution</Button>
                 </form>
             </div>
         )
