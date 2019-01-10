d8fef3350319ce9185cb7a10656d36db467b7cfc
Gerson <g_osio@hotmail.com>
2019-01-09 18:14:49 -0700

    css cards

--- src/Components/Views/Dashboard/Dashboard.js
+++ src/Components/Views/Dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import RenderCont from '../View_Story/RenderCont'
 import { Typography } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import {Link} from "react-router-dom";
+
 
 class Dashboard extends Component {
 
@@ -26,21 +28,23 @@ class Dashboard extends Component {
     render(){
         const stories = this.state.stories.map((story)=>{
             return(
-                <Card className="title">
+                <Card className="storybox">
                 <CardContent>
                 <Typography className= "storyname" key={story.story_id}>{story.title}</Typography>
                 <Typography className = "description" key={story.story_id}>{story.description}</Typography>
+                <Link to ={`/view_story/${story.story_id}`}><button className="view">View story</button></Link>
                 </CardContent>
                 </Card>
             )
         })
         return(
+
             <div className ="dashboard" >
-                {stories}
+               <h1>Stories in the Works</h1>
+             {stories}
             </div>
  
         )
     }
  }
  export default Dashboard;
-
