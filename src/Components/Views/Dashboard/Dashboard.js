981ad93075ef3cf9b0fb392e3139bf1e1e367a80
Justin Valdez <justinjvaldez@gmail.com>
2019-02-04 20:18:52 -0700

    search bar MUI

--- src/Components/Views/Dashboard/Dashboard.js
+++ src/Components/Views/Dashboard/Dashboard.js
@@ -1,131 +1,118 @@
-import React, { Component } from "react";
-import axios from "axios";
-import "./Dashboard.css";
-import RenderCont from "../View_Story/RenderCont";
-import { Typography } from "@material-ui/core";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import axios from 'axios';
+import './Dashboard.css';
+import RenderCont from '../View_Story/RenderCont'
+import { Typography } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import {Link} from "react-router-dom";
 import Snackbar from "@material-ui/core/Snackbar";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
-import ErrorModal from "../ErrorModal/ErrorModal";
-import LandingModal from "../Dashboard/LandingModal";
-import TextField from "@material-ui/core/TextField";
+import ErrorModal from '../ErrorModal/ErrorModal';
+import LandingModal from '../Dashboard/LandingModal';
+import TextField from '@material-ui/core/TextField';
 
 class Dashboard extends Component {
+
   constructor(props) {
-    super(props);
-    this.state = {
-      filteredStories: [],
-      stories: [],
-      open: true
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
+      super(props);
+      this.state = {
+          filteredStories: [],
+          stories: [],
+          open: true
+      }
+     this.handleChange=this.handleChange.bind(this);
+
   }
   handleClose = () => {
-    this.setState({ open: false });
-  };
+      this.setState({ open: false });
+    };
 
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  handleKeyPress = event => {
-    if (event.key === "Enter") {
-      this.login();
-    }
-  };
-     componentDidMount(){
-        axios.get(`/api/Dashboard`)
-        .then(res=>{
-            const stories =res.data;
-            this.setState({stories,  filteredStories:stories});
-        })
-        .catch(err =>{
-            this.setState({serverErrorMessage:' Server error'})
-          });
-    }
-    handleChange(e){
-        this.setState({
-            filteredStories : this.state.stories.filter((story)=>{
-               return story.title.toUpperCase().includes(e.target.value.toUpperCase()) 
-            })
-        })
-    }
-    
-
-
-  handleChange(e){
- this.setState({
-     filteredStories : this.state.stories.filter((story)=>{
-        return story.title.toUpperCase().includes(e.target.value.toUpperCase()) 
-     })
- })
-}
-          
-
-  componentDidMount() {
-    axios
-      .get(`/api/Dashboard`)
-      .then(res => {
-        const stories = res.data;
-        this.setState({ stories });
-      })
-      .catch(err => {
-        this.setState({ serverErrorMessage: " Server error" });
-      });
-  }
-
-       const stories = this.state.filteredStories.map((story)=>{
-        
-           return(
-               <Card className="storybox" key={story.story_id}>
-               <CardContent>
-               <div  className= "storyname">{story.title}</div>
-               <div  className = "description">{story.description}</div>
-               <Link to ={`/view_story/${story.story_id}`}><button className="view">View story</button></Link>
-               
-               </CardContent>
-               </Card>
-           )
-           },
-           <div className="idk">
-           <div className="dashboard">
-            {stories}
-            <Snackbar
-           anchorOrigin={{
-             vertical: "bottom",
-             horizontal: "left",
-           }}
-           open={this.state.open}
-           autoHideDuration={6000}
-           onClose={this.handleClose}
-           message={<p>This is your dashboard. Choose your actions wisely!</p>}
-           action={[
-             <IconButton
-               key="close"
-               aria-label="Close"
-               color="inherit"
-               onClick={this.handleClose}
-             >
-               <CloseIcon />
-             </IconButton>
-           ]}
-         />
-           </div>
-           <input className="filter" type="text" placeholder="Search by Title" onChange={this.handleChange} margin="normal"
-              variant="outlined"
-              border=""
-              style={{backgroundColor: "#EAFBF7", color:"#378674", borderRadius:5}} />
-           <LandingModal/>
-           <h3 className= "storydash">Stories Dashboard</h3>
-</div>
-</div>
-       )
+    componentDidMount(){
+       axios.get(`/api/Dashboard`)
+       .then(res=>{
+           const stories =res.data;
+           this.setState({stories,  filteredStories:stories});
+       })
+       .catch(err =>{
+           this.setState({serverErrorMessage:' Server error'})
+         });
    }
+   handleChange(e){
+       this.setState({
+           filteredStories : this.state.stories.filter((story)=>{
+              return story.title.toUpperCase().includes(e.target.value.toUpperCase())
+           })
+       })
+   }
+
+
+
+ handleChange(e){
+this.setState({
+    filteredStories : this.state.stories.filter((story)=>{
+       return story.title.toUpperCase().includes(e.target.value.toUpperCase())
+    })
+})
+}
+
+
+  render(){
+
+      const stories = this.state.filteredStories.map((story)=>{
+
+          return(
+              <Card className="storybox" key={story.story_id}>
+              <CardContent>
+              <div  className= "storyname">{story.title}</div>
+              <div  className = "description">{story.description}</div>
+              <Link to ={`/view_story/${story.story_id}`}><button className="view">View story</button></Link>
+
+              </CardContent>
+              </Card>
+          )
+      })
+      return(
+          <div className="idk">
+          <div className="dashboard">
+           {stories}
+           <Snackbar
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left"
+          }}
+          open={this.state.open}
+          autoHideDuration={6000}
+          onClose={this.handleClose}
+          message={<p>This is your dashboard. Choose your actions wisely!</p>}
+          action={[
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              onClick={this.handleClose}
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
+          </div>
+          <TextField
+          id="outlined-search"
+          label="Search by Title"
+          type="search"
+          className="filter"
+          margin="normal"
+          variant="outlined"
+          style={{backgroundColor: "#EAFBF7", color:"#378674", borderRadius:5, textAlign:"center",}}
+        />
+
+          
+          <LandingModal/>
+          <h3 className= "storydash">Stories Dashboard</h3>
+</div>
+      )
+  }
 }
 export default Dashboard;
